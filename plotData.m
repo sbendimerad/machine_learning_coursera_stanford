2418fc9c274433e84fea351a46dbf487f90ce7ac
Sabrine <sabrine.bendimerad1@gmail.com>
2020-08-14 15:42:44 +0200

    Add files via upload

--- plotData.m
+++ plotData.m
@@ -1,14 +1,19 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+% Create New Figure
+figure; hold on;
 
-ylabel('Profit in $10,000s'); % Set the y ? axis label
-xlabel('Population of City in 10,000s'); % Set the x ? axis label
+% Find Indices of Positive and Negative Examples
+pos = find(y == 1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
+'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
 
-figure; % open a new figure window
+hold off;
 
 end
